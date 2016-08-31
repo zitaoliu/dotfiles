f86582ae3b03d8b48a58ed152fca251964d5448e
Jerry Zitao Liu <zitao.jerry.liu@gmail.com>
2016-08-30 23:56:43 -0700

    add bash_profile, install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
+
+
+ln -s ~/dotfiles/tmux ~/.tmux
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
