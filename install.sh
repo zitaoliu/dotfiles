132b332ba476483a30d344b761995b2c2d7a5ca9
Jerry Zitao Liu <zitao.jerry.liu@gmail.com>
2018-06-15 23:23:00 +0800

    update dir

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-ln -si ~/dotfiles/vimrc ~/.vimrc
-ln -si ~/dotfiles/vim ~/.vim
+ln -si ~/code/dotfiles/vimrc ~/.vimrc
+ln -si ~/code/dotfiles/vim ~/.vim
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
 git clone https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 git clone https://github.com/fholgado/minibufexpl.vim.git ~/.vim/bundle/minibufexpl.vim
 
-ln -si ~/dotfiles/tmux ~/.tmux
-ln -si ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -si ~/code/dotfiles/tmux ~/.tmux
+ln -si ~/code/dotfiles/tmux.conf ~/.tmux.conf
 
 ln -si "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl2
 
 ln -si "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 
-ln -si ~/dotfiles/bash_profile ~/.bash_profile
+ln -si ~/code/dotfiles/bash_profile ~/.bash_profile
